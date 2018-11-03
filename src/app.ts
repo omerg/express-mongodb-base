872b557f17bf8d78e8788ed75066df715e3e65da
Ömer Gürarslan <omerg@lucidcode.com.tr>
2018-11-03 23:41:32 +0300

    test wip

--- src/app.ts
+++ src/app.ts
@@ -108,7 +108,7 @@ class App {
         // middleware to use for all requests
         this.app.use(function (req, res, next) {
             // do logging
-            console.log('Something is happening.');
+            console.log('Something is happening 2.');
             next(); // make sure we go to the next routes and don't stop here
         });
 
