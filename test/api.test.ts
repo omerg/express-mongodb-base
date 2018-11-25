dd517e74e7dea211e9a6254f1e4f6b45d016e6dc
Ömer Gürarslan <omerg@lucidcode.com.tr>
2018-11-25 20:23:38 +0300

    readme updated

---
+++ test/api.test.ts
@@ -0,0 +1,9 @@
+import * as request from "supertest";
+import app from "../src/app";
+
+describe("GET /api", () => {
+    it("should return 200 OK", () => {
+        return request(app).get("/api")
+            .expect(200);
+    });
+});
